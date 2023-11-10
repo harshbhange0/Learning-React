bf1d36a3f8cd062ecd78e55b1021f7c14bb9149d
harshbhnag0 <harshbhange123@gamil.com>
2023-11-10 14:20:43 +0530

    redux todo done

--- redux-and-toolkite/src/App.jsx
+++ redux-and-toolkite/src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import AddTodo from "./components/AddTodo";
+import DisplayTodos from "./components/DisplayTodos";
 
 function App() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <AddTodo />
+    <div className="flex justify-center flex-col items-center h-screen bg-blue-950">
+      <div className="max-w-2xl bg-[#19376D] text-white p-4 rounded-md overflow-hidden shadow-md shadow-[#4477CE] flex justify-center flex-col items-center">
+       
+        <AddTodo />
+        <DisplayTodos />
+      </div>
     </div>
   );
 }
